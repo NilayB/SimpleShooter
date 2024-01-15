337d0056b63a4cb319dfdb55936a78d70120457f
nilay <imnilaybhatt@gmail.com>
2024-01-15 20:58:11 +0530

    Win Condition and screen

--- Source/SimpleShooter/ShooterPlayerController.h
+++ Source/SimpleShooter/ShooterPlayerController.h
@@ -23,6 +23,8 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<class UUserWidget> LoseScreenClass;
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UUserWidget> WinScreenClass;
 
 	FTimerHandle RestartTimer;
 };
